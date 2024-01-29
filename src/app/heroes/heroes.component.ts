c463c4f6d78310f2ed3b04d30e826deef87ff2ef
Alexey Chernyshov <ubiru77@gmail.com>
2024-01-29 14:45:37 +0400

    fixed toaster

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,6 +9,7 @@ import {FormsModule} from '@angular/forms';
 import {Hero} from '../hero';
 import {HEROES} from '../mock-heroes';
 
+
 @Component({
   standalone: true,
   selector: 'app-heroes',
@@ -19,12 +20,15 @@ import {HEROES} from '../mock-heroes';
     NgIf,
     NgFor,
     UpperCasePipe,
+
   ],
 })
 
 export class HeroesComponent {
   heroes = HEROES;
   selectedHero?: Hero;
+
+  constructor(private toastr: ToastrService) {}
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import {bootstrapApplication, provideProtractorTestingSupport} from '@angular/platform-browser';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
 import {AppComponent} from './app/app.component';
+import { provideToastr } from 'ngx-toastr';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideProtractorTestingSupport()],
+  providers: [provideProtractorTestingSupport(),
+    provideAnimations(), 
+    provideToastr(),],
 });
