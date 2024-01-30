7e2f4419fca4877a8e7649756f7fa2ef2acb6f4f
Alexey Chernyshov <ubiru77@gmail.com>
2024-01-30 16:40:44 +0400

    default

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,27 +1,20 @@
 import {Component} from '@angular/core';
-import {
-  NgIf,
-  NgFor,
-  UpperCasePipe,
-} from '@angular/common';
+import {NgFor} from '@angular/common';
+import {HeroDetailComponent} from '../hero-detail/hero-detail.component';
 import {Hero} from '../hero';
 import {HEROES} from '../mock-heroes';
-import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
   standalone: true,
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
-  imports: [
-    HeroDetailComponent,
-    NgFor,
-  ],
+  imports: [HeroDetailComponent, NgFor],
 })
-
 export class HeroesComponent {
   heroes = HEROES;
-  selectedHero?: Hero;
+
+  selectedHero!: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
