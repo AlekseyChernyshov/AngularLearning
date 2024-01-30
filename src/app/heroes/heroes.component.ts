3e09c6bde2fec2a05164e6d718d5953236be13b7
Alexey Chernyshov <ubiru77@gmail.com>
2024-01-30 17:27:02 +0400

    Разъеденил представление героя по нажатию на два компонента

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,6 +8,7 @@ import { ToastrService } from 'ngx-toastr';
 import {FormsModule} from '@angular/forms';
 import {Hero} from '../hero';
 import {HEROES} from '../mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 
 @Component({
@@ -20,7 +21,7 @@ import {HEROES} from '../mock-heroes';
     NgIf,
     NgFor,
     UpperCasePipe,
-
+    HeroDetailComponent
   ],
 })
 
